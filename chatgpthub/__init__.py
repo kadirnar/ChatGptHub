917b0747715744877f4b046569155e2805ff181f
kadirnar <kadir.nar@hotmail.com>
2023-04-19 00:20:43 +0300

    update version

--- chatgpthub/__init__.py
+++ chatgpthub/__init__.py
@@ -3,4 +3,4 @@ from chatgpthub.templates import custom_prompt_template, translate_chatgpt
 from chatgpthub.tools import load_csv_agent, promptlayer_chatgpt
 from chatgpthub.utils import load_openai_key, load_promptlayer_key
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
